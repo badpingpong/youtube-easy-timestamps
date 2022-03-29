e58443aa99a598489736ac7d8f476c5e9935ab00
YoshinoAkira <akirayo.badpingpong@gmail.com>
2022-03-29 18:35:56 +0900

    show form skelton when button is clicked

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,34 @@
-import { printLine } from './modules/print';
+import { printLine } from './modules/print'
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+console.log('Content script works!')
+console.log('Must reload extension for modifications to take effect.')
 
-printLine("Using the 'printLine' function from the Print Module");
+printLine("Using the 'printLine' function from the Print Module")
+
+const TIMESTAMP_FORM_ID = 'timestamp-form'
+const APPEND_TARGET_ID = 'player'
+
+const showTimestampForm = () => {
+  const newElement = document.createElement('div')
+  newElement.innerHTML = 'フォーム'
+  newElement.setAttribute('id', TIMESTAMP_FORM_ID)
+  document.getElementById(APPEND_TARGET_ID).appendChild(newElement)
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(
+    sender.tab
+      ? 'from a content script:' + sender.tab.url
+      : 'from the extension'
+  )
+  if (request.type === 'show-form') {
+    if (document.getElementById(TIMESTAMP_FORM_ID)) {
+      sendResponse({ farewell: 'no! already exists' })
+      console.log('a?')
+    } else {
+      showTimestampForm()
+      sendResponse({ farewell: 'ok' })
+      console.log('a!')
+    }
+  }
+})
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 import logo from '../../assets/img/logo.svg'
 
 const Popup = () => {
+  const showForm = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { type: 'show-form' })
+    })
+  }
   return (
     <Box>
-      <Button onClick={() => console.log('aaa')}>アイウエオ</Button>
+      <Button onClick={showForm}>フォーム表示</Button>
     </Box>
   )
 }
