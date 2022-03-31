ca6a0b6c2a8f55f03dda8767e73791de169d47ad
YoshinoAkira <akirayo.badpingpong@gmail.com>
2022-03-31 14:34:56 +0900

    modify Popup

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,7 +1,17 @@
-import { Text, Button, Center, Heading, VStack, Image } from '@chakra-ui/react'
+import {
+  Text,
+  Box,
+  Button,
+  Center,
+  Heading,
+  VStack,
+  Image,
+} from '@chakra-ui/react'
 import React from 'react'
 import logo from '../../assets/img/logo.svg'
 
+const HINT_TEXT = chrome.i18n.getMessage('ext_popup_hint')
+const SHOW_FORMS_TEXT = chrome.i18n.getMessage('ext_show_form_at_popup')
 const Popup = () => {
   const showForm = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -9,15 +19,17 @@ const Popup = () => {
     })
   }
   return (
-    <VStack py={6}>
-      <Image src={logo} boxSize="100px" alt="logo" />
-      <Heading size="lg" mb={4}>
-        Easy Youtube Timestamps
-      </Heading>
+    <VStack pt={3} spacing={5} bg="tomato" h="100vh">
+      <VStack>
+        <Image src={logo} boxSize="60px" alt="logo" />
+        <Heading size="lg" mb={4}>
+          Easy Youtube Timestamps
+        </Heading>
+      </VStack>
       <Center mb={6}>
         <VStack>
-          <Button onClick={showForm}>フォーム表示</Button>
-          <Text>(うまく表示されない時に押してみてください)</Text>
+          <Button onClick={showForm}>{SHOW_FORMS_TEXT}</Button>
+          <Text>({HINT_TEXT})</Text>
         </VStack>
       </Center>
       <Heading size="xs">Developed by badpingpong</Heading>
